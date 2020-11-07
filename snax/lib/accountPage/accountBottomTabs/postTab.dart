bb8a8e07ef84836090903f5215cb842d9cd7974e
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2020-11-06 17:01:21 -0800

    got rid of error when loading in profile page

--- snax/lib/accountPage/accountBottomTabs/postTab.dart
+++ snax/lib/accountPage/accountBottomTabs/postTab.dart
@@ -3,12 +3,15 @@ import 'package:snax/backend/requests.dart';
 import 'package:snax/feedPage/post.dart';
 import 'package:snax/feedPage/feedPage.dart';
 
+import '../../helpers.dart';
+
 class PostTab extends StatefulWidget {
   @override
   _PostTabState createState() => _PostTabState();
 }
 
-class _PostTabState extends State<PostTab> {
+class _PostTabState extends State<PostTab>
+    with AutomaticKeepAliveClientMixin<PostTab> {
   List<Post> posts;
 
   void getPosts() {
@@ -27,14 +30,26 @@ class _PostTabState extends State<PostTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: ListView.builder(
-      padding: EdgeInsets.only(top: 16),
-      itemCount: posts.length,
-      itemBuilder: (context, index) {
-        Post post = posts[index];
-        return postWidget(context, post);
-      },
-    ));
+    return posts != null
+        ? Expanded(
+            child: ListView.builder(
+            padding: EdgeInsets.only(top: 16),
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              Post post = posts[index];
+              return postWidget(context, post);
+            },
+          ))
+        : Expanded(
+            child: Center(
+              child: CircularProgressIndicator(
+                valueColor:
+                    new AlwaysStoppedAnimation<Color>(SnaxColors.redAccent),
+              ),
+            ),
+          );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
