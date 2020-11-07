9dc4a1d43269b9c21c397b58b9e5a4de89d2b675
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2020-11-06 16:29:27 -0800

    changed appbar in home page

--- snax/lib/homePage/homePage.dart
+++ snax/lib/homePage/homePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loading/indicator/ball_pulse_indicator.dart';
 import 'package:loading/indicator/ball_spin_fade_loader_indicator.dart';
 import 'package:loading/loading.dart';
+import 'package:number_display/number_display.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:snax/barcodeScanner/barcodeAddCode.dart';
 import 'package:snax/barcodeScanner/barcodeScanner.dart';
@@ -10,6 +11,8 @@ import 'package:snax/homePage/specificSnack.dart';
 
 import 'package:snax/backend/backend.dart';
 import 'package:snax/backend/requests.dart';
+
+import '../helpers.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -29,32 +32,53 @@ class _MainPageState extends State<MainPage> {
                     (BuildContext context, bool innerBoxIsScrolled) {
                   return <Widget>[
                     new SliverAppBar(
-                      title: Text("SNAX"),
+                      /*flexibleSpace: Container(
+                          decoration: BoxDecoration(
+                              gradient: SnaxGradients.redBigThings),
+                        ),*/
+                      elevation: 2,
+                      backgroundColor: Colors.white,
+                      title: Text("SNAX",
+                          style: TextStyle(color: SnaxColors.redAccent)),
                       floating: true,
                       pinned: true,
                       snap: true,
                       bottom: TabBar(
-                        tabs: [Tab(text: "Trending"), Tab(text: "Top")],
+                        isScrollable: true,
+                        unselectedLabelColor: SnaxColors.redAccent,
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        indicator: BoxDecoration(
+                            gradient: SnaxGradients.redBigThings,
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                            color: Colors.redAccent),
+                        labelStyle: TextStyle(fontSize: 18),
+                        tabs: [
+                          Container(width: 164, child: Tab(text: "Trending")),
+                          Container(width: 164, child: Tab(text: "Top"))
+                        ],
                       ),
                       actions: <Widget>[
                         IconButton(
-                            icon: const Icon(Icons.search),
+                            icon: Icon(Icons.search),
+                            color: SnaxColors.redAccent,
                             onPressed: () {
-                              /*showSearch(
-                            context: context,
-                            delegate: BarcodeAddSearch(
-                                (SnackSearchResultItem returnSnack) {
-                              chosenSnack =
-                                  SnaxBackend.getSnack(returnSnack.id);
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          ProductPage(item: chosenSnack)));
-                            }));*/
+                              showSearch(
+                                  context: context,
+                                  delegate: BarcodeAddSearch(
+                                      (SnackSearchResultItem
+                                          returnSnack) async {
+                                    chosenSnack = await SnaxBackend.getSnack(
+                                        returnSnack.id);
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => ProductPage(
+                                                item: chosenSnack)));
+                                  }));
                             }),
                         IconButton(
-                            icon: const Icon(Icons.qr_code_scanner),
+                            icon: Icon(Icons.qr_code_scanner),
+                            color: SnaxColors.redAccent,
                             onPressed: () {
                               //Present Widget
                               Navigator.of(context).push(MaterialPageRoute(
@@ -148,6 +172,7 @@ class _TopList extends State<TopList>
 
 Widget getList(BuildContext context, List<SnackItem> snackList) {
   final List<SnackItem> trendingSnacks = snackList;
+  final display = createDisplay(placeholder: '0');
 
   return Expanded(
       child: trendingSnacks != null
@@ -216,6 +241,10 @@ Widget getList(BuildContext context, List<SnackItem> snackList) {
                 );
               })
           : Container(
-              child: Center(child: CircularProgressIndicator()),
+              child: Center(
+                  child: CircularProgressIndicator(
+                valueColor:
+                    new AlwaysStoppedAnimation<Color>(SnaxColors.redAccent),
+              )),
             ));
 }
