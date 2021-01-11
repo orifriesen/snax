3b04605726f455523b71023b221157e0f7e4d2be
brandon <ramirezbrandon077@gmail.com>
2021-01-11 10:01:50 -0800

    Added futurebuilder

--- snax/lib/accountPage/followingPage.dart
+++ snax/lib/accountPage/followingPage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:snax/backend/backend.dart';
+
+import 'package:snax/backend/requests.dart';
 import 'package:snax/accountPage/accountPage.dart';
 
 class FollowingPage extends StatefulWidget {
+  String uid;
   @override
   _FollowingPageState createState() => _FollowingPageState();
 }
@@ -13,6 +18,23 @@ class _FollowingPageState extends State<FollowingPage> {
       appBar: AppBar(
         title: Text('Following'),
       ),
+      body: FutureBuilder(
+        future: SnaxBackend.getFollowing(this.widget.uid),
+        builder: (context, index) {
+          if (index.connectionState == ConnectionState.done) {
+            return ListView(
+              children: [
+                Container(
+                  child: Text("${SnaxBackend.currentUser.name}"),
+                )
+              ],
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
+      // body: postFollowing(this.widget.uid),
     );
   }
 }
