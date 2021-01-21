b94de9e38d44dd666527c26bfd31103378042164
brandon <ramirezbrandon077@gmail.com>
2021-01-20 17:22:36 -0800

    WebView Implementation
    Added WebView, made in-page webviews for Instagram links.

--- snax/lib/accountPage/settingsTabs/aboutPage.dart
+++ snax/lib/accountPage/settingsTabs/aboutPage.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:snax/customIcons/instagram_icons.dart';
+import 'package:snax/web_view_container.dart';
 
-class AboutPage extends StatefulWidget {
-  @override
-  _AboutPageState createState() => _AboutPageState();
+class Links {
+  String link;
+  String name;
+
+  Links({this.link, this.name});
 }
 
-class _AboutPageState extends State<AboutPage> {
+class AboutPage extends StatelessWidget {
+  List<Links> _links = [
+    Links(link: 'https://www.instagram.com/escherwd/', name: 'Escher'),
+    Links(link: 'https://www.instagram.com/orifriesen/', name: 'Ori'),
+    Links(link: 'https://www.instagram.com/waltbringenberg/', name: 'Walt'),
+    Links(link: 'https://www.instagram.com/ramirez.brrandon/', name: 'Brandon')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,41 +63,33 @@ class _AboutPageState extends State<AboutPage> {
               ),
             ),
           ),
+
           //* Section 2
           Container(
             padding: EdgeInsets.only(top: 16, right: 32, bottom: 8, left: 32),
-            child: Center(
-              child: Column(
-                children: [
-                  Text(
-                    "Meet The Team",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Meet The Team",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  _teamSocials(
-                      'Snax',
-                      () => customLaunch(
-                          "https://www.instagram.com/snaxappofficial/")),
-                  _teamSocials(
-                      'Ori',
-                      () => customLaunch(
-                          "https://www.instagram.com/orifriesen/")),
-                  _teamSocials(
-                      'Escher',
-                      () =>
-                          customLaunch("https://www.instagram.com/escherwd/")),
-                  _teamSocials(
-                      'Walt',
-                      () => customLaunch(
-                          "https://www.instagram.com/waltbringenberg/")),
-                  _teamSocials(
-                      'Brandon',
-                      () => customLaunch(
-                          "https://www.instagram.com/ramirez.brrandon/")),
-                ],
-              ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: _links
+                      .map(
+                          (link) => _teamSocials(context, link.name, link.link))
+                      .toList(),
+                ),
+              ],
             ),
           )
         ],
@@ -95,7 +97,7 @@ class _AboutPageState extends State<AboutPage> {
     );
   }
 
-  Widget _teamSocials(String name, customLaunch()) {
+  Widget _teamSocials(BuildContext context, String name, String url) {
     return Padding(
       padding: const EdgeInsets.only(left: 8, top: 16, bottom: 16, right: 8),
       child: Container(
@@ -113,27 +115,33 @@ class _AboutPageState extends State<AboutPage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              name,
-              style: TextStyle(fontSize: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 15),
+              ),
             ),
+            //* Instagram
             FlatButton(
               onPressed: () {
-                customLaunch();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => WebViewContainer(url)));
               },
-              child: Text("Instagram"),
+              child: Icon(
+                Instagram.icons8_instagram,
+                size: 30,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              minWidth: 5,
             ),
           ],
         ),
       ),
     );
-  }
-
-  customLaunch(command) async {
-    if (await canLaunch(command)) {
-      await launch(command);
-    } else {
-      print("Command did not work");
-    }
   }
 }
