58cb6eaa8c02369a26297ce04962331e5d49df07
brandon <ramirezbrandon077@gmail.com>
2021-01-25 13:50:10 -0800

    minor

--- snax/lib/accountPage/globalAccountPage.dart
+++ snax/lib/accountPage/globalAccountPage.dart
@@ -483,7 +483,7 @@ class _GlobalAccountPageState extends State<GlobalAccountPage>
                   ),
                   onPressed: () => {
                     customLaunch(
-                        "mailto:thesnaxofficial@gmail.com?subject=Reporting%20a%20User:${this.widget.user.username}&body=Reason:"),
+                        "mailto:thesnaxofficial@gmail.com?subject=Reporting%20a%20User: ${this.widget.user.username}&body=Reason: "),
                     Navigator.pop(context),
                   },
                 )
