af5b09bc4f463e535d30dd55f92df560e67c05c7
brandon <ramirezbrandon077@gmail.com>
2021-01-25 23:44:19 -0800

    Removed Expanded from reviewed

--- snax/lib/accountPage/accountBottomTabs/secondTab.dart
+++ snax/lib/accountPage/accountBottomTabs/secondTab.dart
@@ -4,7 +4,8 @@ import 'package:sup/quick_sup.dart';
 class ReviewedTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Padding(
+      padding: const EdgeInsets.only(top: 44),
       child: QuickSup.error(
         image: Center(child: Text("🚧", style: TextStyle(fontSize: 32))),
         title: "Coming Soon",
